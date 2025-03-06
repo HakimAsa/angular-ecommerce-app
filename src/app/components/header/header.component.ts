f136a4cd1be1884bb3e69045a51034f7a6bd182f
aamidou <akimayena26@gmail.com>
2025-03-06 17:51:20 +0100

    adding cart service for adding product to a cart

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,8 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  // cart = signal('Cart');
+  cartService = inject(CartService);
   handleBtnClicked() {
-    alert('btn clicked');
+    console.log('btn clicked');
   }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, signal } from '@angular/core';
+import { Product } from '../models/products.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cart = signal<Product[]>([]);
+
+  //add product to cart
+  addToCart(product: Product) {
+    this.cart.set([...this.cart(), product]);
+  }
+
+  constructor() {}
+}
