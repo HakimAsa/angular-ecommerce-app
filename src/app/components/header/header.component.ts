718e81e2f9e67f41821510d16e6836c7c506bd0e
aamidou <akimayena26@gmail.com>
2025-03-06 13:10:36 +0100

    added primary btn and convert scss to css for tailwind compatibility

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  handleBtnClicked() {
+    alert('btn clicked');
+  }
 }
