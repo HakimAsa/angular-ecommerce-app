4b26df4b66555c3515395788c1bf593b65452b4c
aamidou <akimayena26@gmail.com>
2025-03-15 10:29:25 +0100

    add location functionality to header similar to amazon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  OnInit,
+  Output,
+  signal,
+} from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 import { CartService } from '../../services/cart.service';
 import { RouterLink } from '@angular/router';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { SearchService } from '../../services/search.service';
+import { LocationService } from '../../services/location.service';
 
 @Component({
   selector: 'app-header',
@@ -11,12 +19,38 @@ import { SearchService } from '../../services/search.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  location: string = 'Fetching location...';
+  suburb: string = 'Fetching suburb...';
   searchService = inject(SearchService);
   cartService = inject(CartService);
+
+  constructor(private locationService: LocationService) {}
 
   onSearchInput(searchValue: string) {
     console.log('Search Term:', searchValue);
     this.searchService.setSearchTerm(searchValue);
+  }
+
+  ngOnInit(): void {
+    this.locationService.getUserLocation().then((coords) => {
+      this.location = `Lat: ${coords.latitude}, Lng: ${coords.longitude}`;
+      this.getMyLocation(coords.latitude, coords.longitude);
+    });
+  }
+  getMyLocation(lat: number, lng: number): void {
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.address);
+        this.location = data.address.country || 'Unknown location';
+        // data.address.city || data.address.town || 'Unknown location';
+        this.suburb = data.address.suburb || 'Unknown suburb';
+      })
+      .catch((error) => {
+        console.error('Error fetching city name:', error);
+      });
   }
 }
